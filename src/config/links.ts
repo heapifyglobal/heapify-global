4e9230f61cfe4cf582df31cf0b314767b3276548
Your Name <you@example.com>
2026-01-03 19:39:45 +0530

    third commit

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,8 +1,8 @@
 export const GOOGLE_FORM_LINK = "https://forms.gle/EAueaSUV7mJHfYgn6";
 
 export const SOCIAL_LINKS = {
-  linkedin: "https://www.linkedin.com/in/utkarsh-chaudhary-b3b99128a/",
-  instagram: "https://www.instagram.com/utkrrsh9/",
+  linkedin: "https://www.linkedin.com/in/heapifyglobal/",
+  instagram: "https://www.instagram.com/heapifyglobal/",
   twitter: "https://twitter.com/heapifyglobal",
   facebook: "https://www.facebook.com/@heapifyglobal",
 };
